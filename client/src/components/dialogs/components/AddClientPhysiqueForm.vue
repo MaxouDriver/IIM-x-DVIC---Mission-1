75997e2ef16eb3a6adaec9c3a2aa6556bd5cbfac
maxime <maxime@DESKTOP-F88RN8T.home>
2020-04-19 16:18:53 +0200

    Added model for physical client form

--- client/src/components/dialogs/components/AddClientPhysiqueForm.vue
+++ client/src/components/dialogs/components/AddClientPhysiqueForm.vue
@@ -4,30 +4,37 @@
             <v-col cols="12" sm="12" md="12">
                 <v-text-field
                     label="Company name*"
-                    persistent-hint
+                    persistent-hint 
+                    v-model="model.companyName"
                     required
                 ></v-text-field>
             </v-col>
             <v-col cols="12">
-                <v-text-field label="Addresse" required></v-text-field>
+                <v-text-field label="Addresse" 
+                    v-model="model.address" required></v-text-field>
             </v-col>
             <v-col cols="12" sm="6" md="6">
-                <v-text-field label="Code Postal*" type="number" required></v-text-field>
-            </v-col>
-
-            <v-col cols="12" sm="6" md="6">
-                <v-text-field label="Ville*" required></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="6" md="6">
-                <v-text-field label="Pays*" required></v-text-field>
+                <v-text-field label="Code Postal*" 
+                    v-model="model.postCode" type="number" required></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="6" md="6">
-                <v-text-field label="Tel*" required></v-text-field>
+                <v-text-field label="Ville*" 
+                    v-model="model.city" required></v-text-field>
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Pays*" 
+                    v-model="model.country" required></v-text-field>
+            </v-col>
+
+            <v-col cols="12" sm="6" md="6">
+                <v-text-field label="Tel*" 
+                    v-model="model.tel" required></v-text-field>
             </v-col>
 
             <v-col cols="12" sm="12" md="12">
-                <v-text-field label="Email*" required></v-text-field>
+                <v-text-field label="Email*" 
+                    v-model="model.email" required></v-text-field>
             </v-col>
         </v-row>
     </v-container>
@@ -36,9 +43,8 @@
 <script> 
   export default { 
     name: 'AddClientPhysiqueForm',
-    data () {
-      return {
-      }
+    props:{
+      model: Object
     },
   }
 </script>
